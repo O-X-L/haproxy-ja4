2be153b3912ccbc138285691dc9c462b26781529
Rath Pascal <rath@oxl.at>
2025-06-07 18:56:18 +0200

    exclude GREASE from signature-algorithms (#20, #18)

--- ja4.lua
+++ ja4.lua
@@ -144,7 +144,7 @@ end
 
 local function signature_algorithms(txn)
     -- https://github.com/FoxIO-LLC/ja4/blob/main/python/common.py#L147
-    return split_string(string.lower(txn.c:be2hex(txn.f:ssl_fc_sigalgs_bin(), '-', 2)), '-')
+    return split_string(string.lower(txn.c:be2hex(txn.f:ssl_fc_sigalgs_bin(1), '-', 2)), '-')
 end
 
 local function extensions_signature_merged(txn)
